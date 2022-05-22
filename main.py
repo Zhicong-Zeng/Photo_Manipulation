ff2b9c6df2bedf3063fec703d18bd41342082f02
Zhicong Zeng <zzeng1@seattleu.edu>
2022-05-22 14:15:25 -0700

    Final implemented.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from email.mime import image
 from fastapi import FastAPI, UploadFile
-from fastapi.responses import RedirectResponse, StreamingResponse ,JSONResponse
-import aiofiles , cv2 , io, numpy, base64
-
+from fastapi.responses import RedirectResponse, StreamingResponse
+import aiofiles , cv2 , io, numpy
 
 
 app = FastAPI()
@@ -20,6 +18,7 @@ async def upload_file(file : UploadFile | None = None):
     elif file.content_type != "image/jpeg" :
         return {"Must be in jpg or jpeg, Here is your image type": file.content_type}
     else:
+        # Save as count++.jpg way
         new_filename = str(count[0]) + ".jpg"
         file_location = f"images/{new_filename}"
         count[0] = count[0] + 1 
@@ -44,18 +43,15 @@ async def image_Flip(number, type):
         target_filename = str(number) + ".jpg"
         file_location = f"images/{target_filename}"
         data_image = cv2.imread(file_location)
-
         image = cv2.flip(data_image, 0)
-
         res, res_data = cv2.imencode(".jpg", image)
         return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
+
     elif type == "h" :         #means Horizontally
         target_filename = str(number) + ".jpg"
         file_location = f"images/{target_filename}"
         data_image = cv2.imread(file_location)
-
         image = cv2.flip(data_image, 1)
-
         res, res_data = cv2.imencode(".jpg", image)
         return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -65,12 +61,10 @@ async def image_Rotate(number, degree):
     target_filename = str(number) + ".jpg"
     file_location = f"images/{target_filename}"
     data_image = cv2.imread(file_location)
-
     height, width = data_image.shape[:2]
     center = (width/2, height/2)
     rotate_matrix = cv2.getRotationMatrix2D(center=center, angle = int(degree), scale=1)
     image = cv2.warpAffine(src = data_image, M = rotate_matrix, dsize=(width, height))
-
     res, res_data = cv2.imencode(".jpg", image)
     return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -80,9 +74,7 @@ async def image_Fixed_Grayscale(number):
     target_filename = str(number) + ".jpg"
     file_location = f"images/{target_filename}"
     data_image = cv2.imread(file_location)
-
     image = cv2.cvtColor(data_image, cv2.COLOR_BGR2GRAY)
-
     res, res_data = cv2.imencode(".jpg", image)
     return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -92,10 +84,8 @@ async def image_Specified_Grayscale(number, Red, Green, Blue):
     target_filename = str(number) + ".jpg"
     file_location = f"images/{target_filename}"
     data_image = cv2.imread(file_location)
-
     gray =  (int(Red) + int(Green) + int(Blue))/3
     image = cv2.cvtColor(data_image, int(gray))
-
     res, res_data = cv2.imencode(".jpg", image)
     return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -105,11 +95,9 @@ async def image_Saturate(number, factor : float):
     target_filename = str(number) + ".jpg"
     file_location = f"images/{target_filename}"
     data_image = cv2.imread(file_location)
-
     mean = numpy.uint8(cv2.mean(cv2.cvtColor(data_image, cv2.COLOR_BGR2GRAY))[0])
     img_deg = numpy.ones_like(numpy) * mean
     image = cv2.addWeighted(data_image, factor, img_deg, 1 - factor, 0.0)
-
     res, res_data = cv2.imencode(".jpg", image)
     return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -119,9 +107,7 @@ async def image_Resize_X_Y(number, X : int , Y : int):
     target_filename = str(number) + ".jpg"
     file_location = f"images/{target_filename}"
     data_image = cv2.imread(file_location)
-
     image = cv2.resize(data_image, (X, Y))
-
     res, res_data = cv2.imencode(".jpg", image)
     return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -131,11 +117,9 @@ async def image_Resize_Percent(number, percent : float):
     target_filename = str(number) + ".jpg"
     file_location = f"images/{target_filename}"
     data_image = cv2.imread(file_location)
-
     new_width = int(data_image.shape[1] * (percent/100))
     new_height = int(data_image.shape[0] * (percent/100))
     image = cv2.resize(data_image, (new_width, new_height))
-
     res, res_data = cv2.imencode(".jpg", image)
     return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -145,9 +129,7 @@ async def image_thumbnail(number):
     target_filename = str(number) + ".jpg"
     file_location = f"images/{target_filename}"
     data_image = cv2.imread(file_location)
-
     image = cv2.resize(data_image,(256,256))
-
     res, res_data = cv2.imencode(".jpg", image)
     return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg") 
 
@@ -157,9 +139,7 @@ async def image_Rotate_left(number):
     target_filename = str(number) + ".jpg"
     file_location = f"images/{target_filename}"
     data_image = cv2.imread(file_location)
-
     image = cv2.rotate(data_image, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
-
     res, res_data = cv2.imencode(".jpg", image)
     return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")     
 
@@ -169,9 +149,7 @@ async def image_Rotate_right(number):
     target_filename = str(number) + ".jpg"
     file_location = f"images/{target_filename}"
     data_image = cv2.imread(file_location)
-
     image = cv2.rotate(data_image, cv2.cv2.ROTATE_90_CLOCKWISE)
-
     res, res_data = cv2.imencode(".jpg", image)
     return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")       
 
@@ -186,12 +164,10 @@ async def api_flip(file : UploadFile , type):
         content = await file.read()
         nparr = numpy.fromstring(content, numpy.uint8)
         data_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
         if type == "v":
             image = cv2.flip(data_image, 0)
         elif type =="h":
             image = cv2.flip(data_image, 1)
-
         res, res_data = cv2.imencode(".jpg", image)
         return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -206,12 +182,10 @@ async def api_Rotate(file : UploadFile , degree):
         content = await file.read()
         nparr = numpy.fromstring(content, numpy.uint8)
         data_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
         height, width = data_image.shape[:2]
         center = (width/2, height/2)
         rotate_matrix = cv2.getRotationMatrix2D(center=center, angle = int(degree), scale=1)
         image = cv2.warpAffine(src = data_image, M = rotate_matrix, dsize=(width, height))
-
         res, res_data = cv2.imencode(".jpg", image)
         return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
     
@@ -226,9 +200,7 @@ async def api_FixedGrayscale(file : UploadFile):
         content = await file.read()
         nparr = numpy.fromstring(content, numpy.uint8)
         data_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
         image = cv2.cvtColor(data_image, cv2.COLOR_BGR2GRAY)
-
         res, res_data = cv2.imencode(".jpg", image)
         return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -243,10 +215,8 @@ async def api_SpecifiedGrayscale(file : UploadFile, Red, Green, Blue):
         content = await file.read()
         nparr = numpy.fromstring(content, numpy.uint8)
         data_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
         gray =  (int(Red) + int(Green) + int(Blue))/3
         image = cv2.cvtColor(data_image, int(gray))
-
         res, res_data = cv2.imencode(".jpg", image)
         return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -261,11 +231,9 @@ async def api_Saturate(file : UploadFile, factor : float):
         content = await file.read()
         nparr = numpy.fromstring(content, numpy.uint8)
         data_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
         mean = numpy.uint8(cv2.mean(cv2.cvtColor(data_image, cv2.COLOR_BGR2GRAY))[0])
         img_deg = numpy.ones_like(numpy) * mean
         image = cv2.addWeighted(data_image, factor, img_deg, 1 - factor, 0.0)
-
         res, res_data = cv2.imencode(".jpg", image)
         return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -280,9 +248,7 @@ async def api_Resize_X_Y(file : UploadFile, X : int , Y : int):
         content = await file.read()
         nparr = numpy.fromstring(content, numpy.uint8)
         data_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
         image = cv2.resize(data_image, (X, Y))
-
         res, res_data = cv2.imencode(".jpg", image)
         return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -297,11 +263,9 @@ async def api_Resize_Percent(file : UploadFile, percent : float):
         content = await file.read()
         nparr = numpy.fromstring(content, numpy.uint8)
         data_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
         new_width = int(data_image.shape[1] * (percent/100))
         new_height = int(data_image.shape[0] * (percent/100))
         image = cv2.resize(data_image, (new_width, new_height))
-
         res, res_data = cv2.imencode(".jpg", image)
         return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -316,9 +280,7 @@ async def api_thumbnail(file : UploadFile):
         content = await file.read()
         nparr = numpy.fromstring(content, numpy.uint8)
         data_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
         image = cv2.resize(data_image,(256,256))
-
         res, res_data = cv2.imencode(".jpg", image)
         return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -333,9 +295,7 @@ async def api_Rotate_left(file : UploadFile):
         content = await file.read()
         nparr = numpy.fromstring(content, numpy.uint8)
         data_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
         image = cv2.rotate(data_image, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
-
         res, res_data = cv2.imencode(".jpg", image)
         return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
 
@@ -350,9 +310,7 @@ async def api_Rotate_right(file : UploadFile):
         content = await file.read()
         nparr = numpy.fromstring(content, numpy.uint8)
         data_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
         image = cv2.rotate(data_image, cv2.cv2.ROTATE_90_CLOCKWISE)
-
         res, res_data = cv2.imencode(".jpg", image)
         return StreamingResponse(io.BytesIO(res_data.tobytes()), media_type="image/jpg")
     
